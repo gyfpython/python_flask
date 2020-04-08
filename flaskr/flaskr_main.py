6cda0268ee52bdcee8f13c87b3449369d5cca270
yafei.guo <yafei.guo@ygomi.com>
2020-04-08 19:55:08 +0800

    add flask web app

---
+++ flaskr/flaskr_main.py
@@ -0,0 +1,69 @@
+# all the imports
+import hashlib
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+from flaskr import configration
+from flaskr.mysql_operation.mysql_connection import MysqlConnection
+
+app = Flask(__name__)
+app.config.from_object(configration)
+
+
+db = MysqlConnection(host='127.0.0.1', username='root', password='root', database='flask')
+
+
+@app.route('/')
+def show_entries():
+    result = db.select_data("select title, text from entries order by id desc")
+    # cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in result]
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    print(request.form['title'], request.form['text'])
+    sql = "insert into entries (title, text, updateBy) values ('{title}', '{text}', '{user}')"
+    sql1 = sql.format(title=request.form['title'], text=request.form['text'], user=app.config['USERNAME'])
+    db.connect_db(sql1)
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        user_pwd = hashlib.md5(request.form['password'].encode()).hexdigest()
+        print(user_pwd)
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif user_pwd != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/get_all_rows')
+def get_all_rows():
+    result = db.select_data("SELECT value FROM test_table")
+    results = []
+    for raw in result:
+        results.append(raw[0])
+    return '/'.join(results)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ flaskr/mysql_operation/mysql_connection.py
@@ -0,0 +1,48 @@
+import pymysql
+
+
+class MysqlConnection(object):
+    def __init__(
+            self, host: str = None, port: int = 3306, username: str = None, password: str = None, database: str = None):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+
+    def create_db(self):
+        pass
+
+    def connect_db(self, command: str):
+        if not command:
+            assert "command or database error"
+        database = pymysql.connect(
+            host=self.host, port=self.port, user=self.username, password=self.password, database=self.database)
+        cursor = database.cursor()
+        cursor.execute(command)
+        database.commit()
+        database.close()
+
+    def select_data(self, command: str):
+        if not command:
+            assert "command or database error"
+        database = pymysql.connect(
+            host=self.host, port=self.port, user=self.username, password=self.password, database=self.database)
+        cursor = database.cursor()
+        cursor.execute(command)
+        results = cursor.fetchall()
+        database.close()
+        return list(results)
+
+    def create_table(self, table_name):
+        pass
+
+    def insert_raw(self, command: str):
+        self.connect_db(command)
+
+    def delete_raw(self):
+        pass
+
+
+# test1 = MysqlConnection(host='127.0.0.1', username='root', password='root')
+# print(test1.select_data("SELECT * FROM test_table", 'flask'))
