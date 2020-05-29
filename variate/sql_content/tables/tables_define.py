684e8abbfe8ba4de675a5a35672195274f99fecb
yafei.guo <yafei.guo@ygomi.com>
2020-05-29 16:30:14 +0800

    add define tables

--- variate/sql_content/tables/tables_define.py
+++ variate/sql_content/tables/tables_define.py
@@ -61,3 +61,67 @@ class Catalogs(db.Model):
         self.catalogName = catalog_name
         self.catalogNumber = catalog_number
         self.createBy = create_by
+
+
+class Roles(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    roleName = db.Column(db.VARCHAR(45), unique=True)
+    roleCode = db.Column(db.Integer, unique=True)
+    description = db.Column(db.VARCHAR(45))
+    createBy = db.Column(db.VARCHAR(45))
+    createTime = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __init__(self, role_name: str, role_code: int, create_by: str, description: str):
+        self.roleName = role_name
+        self.roleCode = role_code
+        self.createBy = create_by
+        self.description = description
+
+
+class Permissions(db.Model):
+    __tablename__ = 'permissions'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    permissionName = db.Column(db.VARCHAR(45), unique=True)
+    permissionCode = db.Column(db.Integer, unique=True)
+    description = db.Column(db.VARCHAR(45))
+    createBy = db.Column(db.VARCHAR(45))
+    createTime = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __init__(self, permission_name: str, permission_code: int, create_by: str, description: str):
+        self.permissionName = permission_name
+        self.permissionCode = permission_code
+        self.createBy = create_by
+        self.description = description
+
+
+class RolePermissions(db.Model):
+    __tablename__ = 'role_permissions'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    permission = db.Column(db.VARCHAR(45))
+    roleCode = db.Column(db.Integer)
+    createBy = db.Column(db.VARCHAR(45))
+    createTime = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __init__(self, permission: str, role_code: int, create_by: str):
+        self.permission = permission
+        self.roleCode = role_code
+        self.createBy = create_by
+
+
+class UserRole(db.Model):
+    __tablename__ = 'user_role'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.VARCHAR(45))
+    roleCode = db.Column(db.Integer)
+    createBy = db.Column(db.VARCHAR(45))
+    createTime = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __init__(self, username: str, role_code: int, create_by: str):
+        self.username = username
+        self.roleCode = role_code
+        self.createBy = create_by
